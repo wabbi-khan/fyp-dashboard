91f3ff68cfbad3b18f34bb20bfa46499867aa5bd
wabbi-khan <kwahab789@gmail.com>
2022-04-16 16:23:56 +0500

    ok

--- pages/employees-management/users/FormFields.jsx
+++ pages/employees-management/users/FormFields.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useField } from "formik";
+import useField from "formik";
 const FormFields = ({ label, ...props }) => {
   const [field, meta] = useField(props);
   return (
