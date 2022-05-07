b66923b3887a12b979015ce3de4d551885647b86
wabbi-khan <kwahab789@gmail.com>
2022-05-07 17:23:27 +0500

    E.M complete with fireBase

--- pages/file-management/config/ImageConfig.jsx
+++ pages/file-management/config/ImageConfig.jsx
@@ -1,18 +1,25 @@
-import fileDefault from "~/public/assets/file-blank-solid-240.png";
-import filecss from "~/public/assets/file-css-solid-240.png";
-import filePdf from "~/public/assets/file-pdf-solid-240.png";
-import filePng from "~/public/assets/file-png-solid-240.png";
-export const ImageConfig = {
-  default: fileDefault,
-  pdf: filePdf,
-  png: filePng,
-  css: filecss,
-};
+// import fileDefault from "~/public/assets/file-blank-solid-240.png";
+// import filecss from "~/public/assets/file-css-solid-240.png";
+// import filePdf from "~/public/assets/file-pdf-solid-240.png";
+// import filePng from "~/public/assets/file-png-solid-240.png";
+// export const ImageConfig = {
+//   default: fileDefault,
+//   pdf: filePdf,
+//   png: filePng,
+//   css: filecss,
+// };
 
+// import React from "react";
+
+// const ImageConfig = () => {
+//   return <div> ImageConfig </div>;
+// };
+
+// export default ImageConfig;
 import React from "react";
 
 const ImageConfig = () => {
-  return <div> ImageConfig </div>;
+  return <div>ImageConfig</div>;
 };
 
 export default ImageConfig;
